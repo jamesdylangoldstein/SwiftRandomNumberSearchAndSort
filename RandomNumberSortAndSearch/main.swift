43a7616337d519fb078e44f907c3e4b7903d6acb
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-20 17:25:52 -0700

    Initial Commit

---
+++ RandomNumberSortAndSearch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RandomNumberSortAndSearch
+//
+//  Created by James Dylan Goldstein on 6/20/16.
+//  Copyright © 2016 James Dylan Goldstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
